42ccae9cb5970ec1247901316b915815e75e5393
Igor Silva <igor@igorsilva.pro>
2025-08-23 10:34:37 +0100

    feat: update Onboarding component to use Button for sign-in

--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -1,13 +1,21 @@
 import { useAuthActions } from '@convex-dev/auth/react';
+import { Button } from '~/components/ui/button';
 
 export function Onboarding() {
 	//
 	const { signIn } = useAuthActions();
 
+	const handleSignIn = () => {
+		console.log('signing in');
+		signIn('google', { redirectTo: location.href });
+	};
+
 	return (
 		<div className="flex flex-col gap-8 my-6 h-full p-4">
 			<div className="text-center space-y-4">
-				<h1 className="text-4xl font-bold tracking-tight">Welcome to Meseeks</h1>
+				<Button onClick={handleSignIn} className="text-4xl font-bold tracking-tight">
+					Welcome to Meseeks
+				</Button>
 			</div>
 			<footer className="absolute bottom-4 text-sm text-muted-foreground flex gap-4">
 				<a
